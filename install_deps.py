a2cd6912f6e3c600101e27eecc6e4747faff4af3
premek <1145361+premek@users.noreply.github.com>
2023-05-15 23:29:35 +0200

    update pink, 34/136 passed

--- install_deps.py
+++ install_deps.py
@@ -158,8 +158,8 @@ DEPS = [
 
     # Pink
     ('deps/pink.zip',
-     'https://github.com/premek/pink/archive/5fc6e7aeda9cd13bac3483e8235dc075f381fac4.zip',
-     '465669fdb1b4c1aed22e61e23686cf44ec8b70f9',
+     'https://github.com/premek/pink/archive/49e715134e2e15c6fde1a69de018d12df7ec7c92.zip',
+     'cdd0b623fb75a1aafe45422ccc0ea1239e304aaa',
      'all'),
 
     # Tachyons
